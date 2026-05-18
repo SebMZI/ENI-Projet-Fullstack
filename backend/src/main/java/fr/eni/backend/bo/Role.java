393d05a4e9659e30e276bd759c236ea50fb555d1
Majinbuh <rogerio.rodrigues.perso@gmail.com>
2026-05-18 17:53:53 +0200

    Fix confict Role

--- backend/src/main/java/fr/eni/backend/bo/Role.java
+++ backend/src/main/java/fr/eni/backend/bo/Role.java
@@ -2,11 +2,9 @@ package fr.eni.backend.bo;
 
 import fr.eni.backend.bo.key.RolePK;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
@@ -19,6 +17,8 @@ import lombok.NoArgsConstructor;
 public class Role {
 
     @Id
+    @NonNull
+    @NotBlank
     @Size(max = 50)
     @Column(name = "USER_REGISTRATION", nullable = false, length = 50)
     private String immatriculation;
