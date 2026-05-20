a67e7d85c8a858be8fa9feebcc409343e0bd587a
Omar-Al-Somah-09 <ragaafiles1@gmail.com>
2026-05-20 11:55:47 +0200

    Refactoring Role

--- backend/src/main/java/fr/eni/backend/bo/Role.java
+++ backend/src/main/java/fr/eni/backend/bo/Role.java
@@ -1,9 +1,6 @@
 package fr.eni.backend.bo;
 
-import fr.eni.backend.bo.key.RolePK;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
@@ -13,22 +10,13 @@ import lombok.*;
 
 @Entity
 @Table(name = "ROLE")
-@IdClass(RolePK.class)
 public class Role {
 
     @Id
-    @NonNull
-    @NotBlank
-    @Size(max = 50)
-    @Column(name = "USER_REGISTRATION", nullable = false, length = 50)
-    private String immatriculation;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ROLE_ID", nullable = false)
+    private Integer id;
 
-    @Id
     @Column(name = "ROLE")
     private String role;
-
-    // Relation ManyToOne vers Utilisateur
-    @ManyToOne
-    @JoinColumn(name = "USER_REGISTRATION", insertable = false, updatable = false)
-    private Utilisateur utilisateur;
 }
